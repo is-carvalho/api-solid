3a1f8de18f2e7fb37acb4cb92a99a45f100e6dbf
is-carvalho <iscarvalho.dev@gmail.com>
2025-11-19 18:42:20 -0300

    refactor: create route file and register controller to achive SRP

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,26 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { prisma } from "@/libs/prisma";
+import z from "zod";
+import { hash } from "bcryptjs";
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  const password_hash = await hash(password, 6);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  });
+
+  return reply.status(201).send();
+}
